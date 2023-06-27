4bdf5e5d1444e3a708f1d0bffe6c632fcaca3080
Pedro <conradohpedro@gmail.com>
2023-06-27 17:43:03 -0300

    refactor Checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,157 +1,36 @@
-import {
-  MapPinLine,
-  CurrencyDollar,
-  CreditCard,
-  Bank,
-  Money,
-  Minus,
-  Plus,
-  Trash,
-} from 'phosphor-react'
-import {
-  AddressInfo,
-  CheckoutContainer,
-  OrderInformation,
-  PaymentInfo,
-  PaymentCard,
-  ClientInformation,
-  InputCep,
-  InputStreet,
-  InputNumber,
-  InputAdjuct,
-  InputCity,
-  InputNeighborhood,
-  InputState,
-  OrderSumary,
-  AmountTotal,
-  OrdemItem,
-  TotalAmount,
-  DeliveryPrice,
-  ItensTotal,
-  Amount,
-  Delete,
-} from './styles'
-import Img from '../../assets/Americano.svg'
-import { NavLink } from 'react-router-dom'
+import { AdressContainer, CheckoutContainer } from './styles'
+
+import { OrderForm } from './components/OrderForm'
+import { CoffeeSelected } from './components/CoffeeSelected'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormProvider, useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { PaymentMethod } from './components/PaymentMethod'
+
+const OrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, 'Campo obrigatório'),
+  rua: zod.string().min(1, 'Campo obrigatório'),
+  numero: zod.number().min(1),
+  complemento: zod.string().min(1, 'Campo obrigatório'),
+  bairro: zod.string().min(1, 'Campo obrigatório'),
+  cidade: zod.string().min(1, 'Campo obrigatório'),
+  uf: zod.string().min(1, 'Campo obrigatório'),
+})
 
 export function Checkout() {
-  return (
-    <CheckoutContainer>
-      <ClientInformation>
-        <h3>Complete seu pedido</h3>
-        <AddressInfo>
-          <header>
-            <MapPinLine size={22} />
-            <div>
-              <span>Pagamento</span>
-              <small>
-                O pagamento é feito na entrega. Escolha a forma que deseja pagar
-              </small>
-            </div>
-          </header>
-          <main>
-            <InputCep type="text" placeholder="CEP" />
-            <InputStreet type="text" placeholder="Rua" />
-            <InputNumber type="text" placeholder="Número" />
-            <InputAdjuct type="text" placeholder="Complemento" />
-            <InputNeighborhood type="text" placeholder="Bairro" />
-            <InputCity type="text" placeholder="Cidade" />
-            <InputState type="text" placeholder="UF" />
-          </main>
-        </AddressInfo>
-        <PaymentInfo>
-          <header>
-            <CurrencyDollar size={22} />
-            <div>
-              <span>Pagamento</span>
-              <small>
-                O pagamento é feito na entrega. Escolha a forma que deseja pagar
-              </small>
-            </div>
-          </header>
-          <main>
-            <PaymentCard>
-              <CreditCard />
-              <span>CARTÃO DE CRÉDITO</span>
-            </PaymentCard>
-            <PaymentCard>
-              <Bank />
-              <span>CARTÃO DE DÉBITO</span>
-            </PaymentCard>
-            <PaymentCard>
-              <Money />
-              <span>DINHEIRO</span>
-            </PaymentCard>
-          </main>
-        </PaymentInfo>
-      </ClientInformation>
+  const confirmOrderForm = useForm({
+    resolver: zodResolver(OrderFormValidationSchema),
+  })
 
-      <OrderInformation>
-        <h3>Cafés selecionados</h3>
-        <OrderSumary>
-          <OrdemItem>
-            <img src={Img} alt="" />
-            <main>
-              Expresso Tradicional
-              <section>
-                <Amount>
-                  <button>
-                    <Minus size={14} />
-                  </button>
-                  <span>1</span>
-                  <button>
-                    <Plus size={14} />
-                  </button>
-                </Amount>
-                <Delete>
-                  <Trash size={16} color="#8047F8" />
-                  <span>REMOVER</span>
-                </Delete>
-              </section>
-            </main>
-            <h3>R$ 9,90</h3>
-          </OrdemItem>
-          <OrdemItem>
-            <img src={Img} alt="" />
-            <main>
-              <p>Expresso Tradicional</p>
-              <section>
-                <Amount>
-                  <button>
-                    <Minus size={14} />
-                  </button>
-                  <span>1</span>
-                  <button>
-                    <Plus size={14} />
-                  </button>
-                </Amount>
-                <Delete>
-                  <Trash size={16} color="#8047F8" />
-                  <span>REMOVER</span>
-                </Delete>
-              </section>
-            </main>
-            <h3>R$ 9,90</h3>
-          </OrdemItem>
-          <AmountTotal>
-            <ItensTotal>
-              <small>Total de itens</small>
-              <span>R$ 29,70</span>
-            </ItensTotal>
-            <DeliveryPrice>
-              <small>Entrega</small>
-              <span>R$ 3,50</span>
-            </DeliveryPrice>
-            <TotalAmount>
-              <h3>Total</h3>
-              <span>R$ 33,20</span>
-            </TotalAmount>
-          </AmountTotal>
-          <NavLink to="/success" title="Success">
-            CONFIMAR PEDIDO
-          </NavLink>
-        </OrderSumary>
-      </OrderInformation>
-    </CheckoutContainer>
+  return (
+    <FormProvider {...confirmOrderForm}>
+      <CheckoutContainer>
+        <AdressContainer>
+          <OrderForm />
+          <PaymentMethod />
+        </AdressContainer>
+        <CoffeeSelected />
+      </CheckoutContainer>
+    </FormProvider>
   )
 }
