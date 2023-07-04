96c6f91779c648eae76759ab1887b2bf315bf847
Pedro <conradohpedro@gmail.com>
2023-07-04 11:50:22 -0300

    refactor Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,11 @@
 import { CoffeeList } from './components/CoffeeList'
 import { StoreDetails } from './components/StoreDetails'
-import { HomeContainer } from './styles'
 
 export function Home() {
   return (
-    <HomeContainer>
+    <>
       <StoreDetails />
       <CoffeeList />
-    </HomeContainer>
+    </>
   )
 }
