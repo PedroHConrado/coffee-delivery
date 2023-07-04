aeeb66a6f9a420fab44702fc58e48c6846394fd9
Pedro <conradohpedro@gmail.com>
2023-07-04 11:43:21 -0300

    add ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Router } from './Router'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/dafault'
 import { CartContextProvider } from './contexts/CartContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-
+      <ToastContainer />
       <BrowserRouter>
         <CartContextProvider>
           <Router />
